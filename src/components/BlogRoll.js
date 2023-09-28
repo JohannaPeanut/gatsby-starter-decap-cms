0dbbf75e0d94607a413ef12253e5b7a35a675b24
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-09-28 15:19:56 +0200

    add field in blog

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -1,11 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql, StaticQuery } from 'gatsby'
-import PreviewCompatibleImage from './PreviewCompatibleImage'
-
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql, StaticQuery } from "gatsby";
+import PreviewCompatibleImage from "./PreviewCompatibleImage";
 
 const BlogRollTemplate = (props) => {
-  
   const { edges: posts } = props.data.allMarkdownRemark;
 
   return (
@@ -15,7 +13,7 @@ const BlogRollTemplate = (props) => {
           <div className="is-parent column is-6" key={post.id}>
             <article
               className={`blog-list-item tile is-child box notification ${
-                post.frontmatter.featuredpost ? 'is-featured' : ''
+                post.frontmatter.featuredpost ? "is-featured" : ""
               }`}
             >
               <header>
@@ -34,7 +32,7 @@ const BlogRollTemplate = (props) => {
                       }}
                     />
                   </div>
-                ) }
+                )}
                 <p className="post-meta">
                   <Link
                     className="title has-text-primary is-size-4"
@@ -42,6 +40,7 @@ const BlogRollTemplate = (props) => {
                   >
                     {post.frontmatter.title}
                   </Link>
+                  <h2>{post.frontmatter.subTitle || "a true story"}</h2>
                   <span> &bull; </span>
                   <span className="subtitle is-size-5 is-block">
                     {post.frontmatter.date}
@@ -60,8 +59,8 @@ const BlogRollTemplate = (props) => {
           </div>
         ))}
     </div>
-  )
-}
+  );
+};
 
 BlogRoll.propTypes = {
   data: PropTypes.shape({
@@ -69,8 +68,7 @@ BlogRoll.propTypes = {
       edges: PropTypes.array,
     }),
   }),
-}
-
+};
 
 export default function BlogRoll() {
   return (
@@ -90,6 +88,7 @@ export default function BlogRoll() {
                 }
                 frontmatter {
                   title
+                  subTitle
                   templateKey
                   date(formatString: "MMMM DD, YYYY")
                   featuredpost
@@ -100,7 +99,6 @@ export default function BlogRoll() {
                         quality: 100
                         layout: CONSTRAINED
                       )
-
                     }
                   }
                 }
